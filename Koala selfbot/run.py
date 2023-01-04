496131741f7b62476afcfc89fa2d8c2a74bc3b67
Knfamous Koala <75808182+Koala-3k@users.noreply.github.com>
2023-01-04 20:45:01 +0530

    Update run.py

--- Koala selfbot/run.py
+++ Koala selfbot/run.py
@@ -2,12 +2,17 @@ import discord
 import os
 from discord.ext import commands
 
-client=commands.Bot(command_prefix=':', self_bot=True, help_command=None, status=discord.Status.idle, intents=discord.Intents.all())
+intents = discord.Intents.default()
+intents.members = True
+
+client=commands.Bot(command_prefix=':', self_bot=True, help_command=None)
+
+x = input("enter token: ")
 
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Streaming(name="Koala Selfbot v1", url="https://twitch.tv/InfamousKoala"))
+    await client.change_presence(activity=discord.Streaming(name="1337", url="https://twitch.tv/infamouskoala"))
     os.system('clear')
-    print(f'Logged in as {client.user} ({client.user.id})')
+    print("Logged in")
 
-client.run("TOKEN", bot=False)
+client.run(x, bot=False)
