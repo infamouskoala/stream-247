5175154f93e6f575944fec64e9d11b432fe82ede
Knfamous Koala <75808182+Koala-3k@users.noreply.github.com>
2023-01-04 20:32:46 +0530

    Update run.py

--- Koala selfbot/run.py
+++ Koala selfbot/run.py
@@ -2,7 +2,7 @@ import discord
 import os
 from discord.ext import commands
 
-client=commands.Bot(command_prefix=':', self_bot=True, help_command=None, status=discord.Status.idle)
+client=commands.Bot(command_prefix=':', self_bot=True, help_command=None, status=discord.Status.idle, intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
