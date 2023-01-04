355f543684c176d79141c026bd19b5b50d42b689
Knfamous Koala <75808182+Koala-3k@users.noreply.github.com>
2023-01-04 19:52:39 +0530

    Add files via upload

---
+++ Koala selfbot/run.py
@@ -0,0 +1,13 @@
+import discord
+import os
+from discord.ext import commands
+
+client=commands.Bot(command_prefix=':', self_bot=True, help_command=None, status=discord.Status.idle)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Streaming(name="Koala Selfbot v1", url="https://twitch.tv/InfamousKoala"))
+    os.system('clear')
+    print(f'Logged in as {client.user} ({client.user.id})')
+
+client.run("TOKEN", bot=False)
